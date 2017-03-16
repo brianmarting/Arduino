5d7c9d8741b8447fe4964f460f26ad592284282b
brianmarting <brian_carting@hotmail.com>
2017-03-16 09:45:55 +0100

    sync

--- MainClass/MainClass.ino
+++ MainClass/MainClass.ino
@@ -1,10 +1,9 @@
 /*This project requires 2 continous rotation servos connected to pins 9 and 10 of arduino*/ 
 
-#include <Servo.h>
-Servo left;//Define left servo
-Servo right;//Define right servo
 byte val;
 int lives = 3;
+char key = 'KEY_RIGHT_ARROW';
+
 
 //DistanceSensor
 float distance;
@@ -34,8 +33,7 @@ const int MotorPin4 = 9; //INPUT 2 Motor2
 void setup()
 {
   Serial.begin(9600);//Change the baud rate value depending on the default baud rate of your bluetooth module, for Bluesmirf-115200 and for JY-MCU-9600
-  left.attach(9, 800, 2200); //left servo on digital pin 9 of arduino
-  right.attach(10, 800, 2200); //right servo on digital pin 10 of arduino
+  
   //Serial.begin(9600);  
   pinMode(ECHOPIN, INPUT); 
   pinMode(TRIGPIN, OUTPUT);
@@ -59,16 +57,65 @@ void setup()
     attachInterrupt(inputIR, readIR, CHANGE);
     attachInterrupt(button, fireIR, FALLING);    
   }
-  
+ 
 }
 
 void loop()
 {  
  CheckAvailable();
  CalculateDistance();
+ //backward();
+ char incomingChar = 0;   // for incoming serial data
+
+  if (Serial.available() > 0) {
+    incomingChar = Serial.read();
+    switch (incomingChar) {
+      case 'w':
+        forward();
+        break;
+      case 'd':
+        turnRight();
+        break;
+      case 's':
+        backward();
+        break;
+      case 'a':
+        turnLeft();
+        break;
+      case 'z':
+        stopp();
+        break;  
+      
+    }
+  }
+ //forward();
 }
 
 
+
+//IR
+//----------------------------------------------------------
+void readIR() {
+  Serial.println("Shot taken...");
+  signalIR = analogRead(inputIR);
+  Serial.println(signalIR);
+  lives--; 
+  Serial.println("Lives : ");
+  Serial.print(lives);
+}
+
+void fireIR() {
+  Serial.println("Shooting...");
+
+  digitalWrite(outputIR, HIGH);
+  delay(100);
+  digitalWrite(outputIR, LOW);
+
+}
+
+
+//Distancesensor
+//----------------------------------------------------------
 void CalculateDistance(){
   // Start Ranging -Generating a trigger of 10us burst 
   digitalWrite(TRIGPIN, LOW); 
@@ -84,7 +131,7 @@ void CalculateDistance(){
   
   Serial.print(distance); 
   Serial.println(" cm");
-  delay(5000); 
+  delay(500); 
   
 }
 
@@ -96,7 +143,7 @@ void CheckAvailable(){
   {
     val=Serial.read();
     Serial.println(int(val));
-    MoveRobot();
+    
     
   }
 }
@@ -135,60 +182,14 @@ void turnLeft() {
 
 }
 
-//TODO
-void MoveRobot(){
-  if(int(val)==49)//Move front
-{
-  right.write(180);//Rotates servo clockwise
-  left.write(0);//Rotates servo anticlockwise
-}
-
-if(int(val)==50)//Move back
-{
-  right.write(0);
-  left.write(180);
-}
-
-if(int(val)==53)//Stop
-{
-  right.write(90);//Stops the servos
-  left.write(90);//Stops the servos
-}
-
-if(int(val)==51)//left
-{
-  right.write(180);
-  left.write(90);
-}
-
-if(int(val)==52)//right
-{
-  right.write(90);
-  left.write(0);
+void stopp(){
+  digitalWrite(MotorPin1, LOW);
+  digitalWrite(MotorPin2, LOW);
+  digitalWrite(MotorPin3, LOW);
+  digitalWrite(MotorPin4, LOW);
 }
 
 
-
-
-}
-
-
-void readIR() {
-  signalIR = analogRead(inputIR);
-  Serial.println(signalIR);
-  lives--; 
-  Serial.println("Lives : ");
-  Serial.print(lives);
-}
-
-void fireIR() {
-  Serial.println("Shooting...");
-
-  digitalWrite(outputIR, HIGH);
-  delay(100);
-  digitalWrite(outputIR, LOW);
-
-}
 
 
 
